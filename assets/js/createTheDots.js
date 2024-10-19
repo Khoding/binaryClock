d650dfd53b62fdda117a19499a81d8de05fcd4c9
Khoding <julien.rutscho@ceff.ch>
2024-10-19 22:15:37 +0200

    relatively beautiful design added show or hide numbers

--- assets/js/createTheDots.js
+++ assets/js/createTheDots.js
@@ -1,34 +1,38 @@
 function removeElements() {
     const myNode = document.getElementById('main');
     myNode.innerHTML = '';
-    
-    document.getElementById('main').classList.toggle('sixbit')
+
+    document.getElementById('main').classList.toggle('sixbit');
 }
 
 function addElement() {
-    let el
-    let idIndex = 0
-    let nbElements = 24
-    let text = [8, 4, 2, 1]
-    let textModulo = 4
+    let el;
+    let elSpan;
+    let idIndex = 0;
+    let nbElements = 24;
+    let text = [8, 4, 2, 1];
+    let textModulo = 4;
 
     if (document.getElementById('main').classList.contains('sixbit')) {
+        text = [32, 16, 8, 4, 2, 1];
         // text = [128, 64 ,32, 16, 8, 4, 2, 1]
+        textModulo = 6;
         // textModulo = 8
-        text = [32, 16, 8, 4, 2, 1]
-        textModulo = 6
-        nbElements = 18
+        nbElements = 18;
+        // nbElements = 24;
     }
 
     for (let i = 0; i < nbElements; i++) {
-        el = document.createElement('div')
-        el.setAttribute('id', idIndex)
-        el.classList.add('case')
-        // el.innerText = text[i%textModulo]
-        
-        document.getElementById("main").appendChild(el)
-        idIndex++
+        el = document.createElement('div');
+        el.setAttribute('id', idIndex);
+        el.classList.add('case');
+        elSpan = document.createElement('span');
+        elSpan.innerText = text[i % textModulo];
+        el.appendChild(elSpan);
+
+        document.getElementById('main').appendChild(el);
+        idIndex++;
     }
 }
 
-addElement()
+addElement();
--- assets/js/theTimePart.js
+++ assets/js/theTimePart.js
@@ -1,30 +1,32 @@
 function addZero(i) {
-    if (i < 10) { i = "0" + i }
+    if (i < 10) {
+        i = '0' + i;
+    }
     return i;
 }
-  
+
 function dec2bin(dec) {
     return (dec >>> 0).toString(2);
 }
 
 function formatBinary(bin) {
-    let indexMaxSize = 4
+    let indexMaxSize = 4;
     if (document.getElementById('main').classList.contains('sixbit')) {
-        indexMaxSize = 6
+        indexMaxSize = 6;
         // indexMaxSize = 8
     }
 
     for (let index = bin.length; index < indexMaxSize; index++) {
         bin = '0' + bin;
-    };
-    return bin
+    }
+    return bin;
 }
-  
+
 let indexToReload = 0;
 
 function changeTheThings() {
     // Get a new Date object every time the function is called
-    const d = new Date(); 
+    const d = new Date();
     let h = addZero(d.getHours()).toString().split('');
     let m = addZero(d.getMinutes()).toString().split('');
     let s = addZero(d.getSeconds()).toString().split('');
@@ -42,33 +44,33 @@ function changeTheThings() {
         formatBinary(dec2bin(m[1])).split(''),
         formatBinary(dec2bin(s[0])).split(''),
         formatBinary(dec2bin(s[1])).split('')
-    ]
+    ];
 
     if (document.getElementById('main').classList.contains('sixbit')) {
         arrayOfThings = [
             formatBinary(dec2bin(h)).split(''),
             formatBinary(dec2bin(m)).split(''),
-            formatBinary(dec2bin(s)).split(''),
-        ]
+            formatBinary(dec2bin(s)).split('')
+        ];
     }
 
     arrayOfThings.forEach(element => {
         element.forEach(el => {
-        if (parseInt(el) === 1) {
-            document.getElementById(indexToReload).classList.add('on')
-        } else {
-            document.getElementById(indexToReload).classList.remove('on')
-        }
+            if (parseInt(el) === 1) {
+                document.getElementById(indexToReload).classList.add('on');
+            } else {
+                document.getElementById(indexToReload).classList.remove('on');
+            }
 
-        indexToReload++
+            indexToReload++;
 
-        if (indexToReload >= arrayOfThings.flat().length) {
-            indexToReload = 0;
-        }
+            if (indexToReload >= arrayOfThings.flat().length) {
+                indexToReload = 0;
+            }
         });
     });
 }
 
-changeTheThings()
+changeTheThings();
 
-setInterval(changeTheThings, 1000)
+setInterval(changeTheThings, 1000);
